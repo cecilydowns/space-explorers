c3f93c1ff921887ac1eb2971caf3c9d678bfb03b
Cecily <cecily.downs@gmail.com>
2018-05-13 11:22:45 -0700

    Add Rock component

---
+++ src/components/Rock.vue
@@ -0,0 +1,31 @@
+<template>
+    <img v-bind:src="require(`../assets/rock${this.type}.svg`)" class="rock" v-bind:style="{ 
+        width: `${this.size}px`,
+        top: this.top ? this.top : '',
+        left: this.left ? this.left : '', 
+        right: this.right ? this.right : '',
+        bottom: this.bottom ? this.bottom : '',
+        animationDelay: `${this.delay}s`,
+        animationDuration: `${this.duration}s`
+        }" />
+</template>
+
+<script>
+export default {
+  name: "Rock",
+  props: {
+    size: Number,
+    top: String,
+    bottom: String,
+    right: String,
+    left: String,
+    type: String
+  }
+};
+</script>
+
+<style scoped>
+.rock {
+  position: absolute;
+}
+</style>
