9cda3724bb70a2ddbe719536a2b906c05a7bf7be
Cecily <cecily.downs@gmail.com>
2018-05-14 16:20:31 -0700

    Add VeeValidate to main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from "vue";
+import VeeValidate from "vee-validate";
+import App from "./App";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VeeValidate);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
